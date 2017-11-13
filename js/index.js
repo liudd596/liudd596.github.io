652a5131f3d5c6c7c11cb802f9cc2daa848de48b
Liudd <13718017521@163.com>
2017-11-13 16:05:53 +0800

    流量充值案例

--- js/index.js
+++ js/index.js
@@ -1,76 +1,49 @@
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
+//常见问题
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
 
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
 
-  var that = this;
-  var delta = 300 - Math.random() * 100;
+		if (!e.data.multiple) {
+			$el.find('.subm').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
 
-  if (this.isDeleting) { delta /= 2; }
-
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
-
-  setTimeout(function() {
-    that.tick();
-  }, delta);
-};
-
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { 10px; border-right: 0.08em solid #009966 }";
-  document.body.appendChild(css);
-};
-
-$(function(){
-  var onClass = "on";
-  var showClass = "show";
-  
-  $("input").bind("checkval",function(){
-    var label = $(this).prev("label");
-    if(this.value !== ""){
-      label.addClass(showClass);
-    } else {
-      label.removeClass(showClass);
-    }
-  }).on("keyup",function(){
-    $(this).trigger("checkval");
-  }).on("focus",function(){
-    $(this).prev("label").addClass(onClass);
-  }).on("blur",function(){
-      $(this).prev("label").removeClass(onClass);
-  }).trigger("checkval");
+	var accordion = new Accordion($('#accordion'), false);
 });
+//充值
+$(document).ready(function() {
+    var choice_amount = $("#amountrule");//充值金额
+    $(choice_amount).children().each(function(){
+        $(this).on("click",function(){
+            $(this).addClass("money-on").siblings().removeClass("money-on");
+        });
+    });
+});
+
+var myreg = /^1[3-8]+\d{9}$/;
+function step1() {
+    var src = $("#phonenum").val();
+    if (myreg.test(src)) {
+        $("#index_errorTip").css("display", "none");
+        window.location.href = 'iphone_pay/Iphone_pay.html';
+    }
+    if (!myreg.test(src)) {
+        $("#index_errorTip").css("display", "block");
+        return;
+    }
+};
